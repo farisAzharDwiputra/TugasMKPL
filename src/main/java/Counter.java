2f43092b4985a186e04f19d0e10fc5f6820e68ae
farisAzharDwiputra <farisazhar.0241@gmail.com>
2025-05-11 22:24:27 +0700

    Testing gagal

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -9,15 +9,15 @@ public class Counter {
 	}
 		
 	public void reset() {
-		count = 0;
+		count = 1;
 	}
 	
 	public void increment() {
-		count++;
+
 	}
 	
 	public void decrement() {
-		count--;
+
 	}
 	
 	public int getCount() {
